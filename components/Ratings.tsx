cf68aab827884c18d598452c9286fc1a1f27fdcc
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-04-16 11:44:51 -0600

    Update rating component props

--- components/Ratings.tsx
+++ components/Ratings.tsx
@@ -7,14 +7,33 @@ const DEFAULT_ICON = "★";
 const DEFAULT_UNSELECTED_COLOR = "gray";
 const DEFAULT_COLOR = "#fadb8c";
 
-export default function Ratings() {
-  const [rating, setRating] = useState<number>(0);
+interface RatingsProps {
+  editable: boolean;
+  initialRating: number;
+}
+
+export default function Ratings({ editable, initialRating }: RatingsProps) {
+  const [rating, setRating] = useState<number>(initialRating);
   const [temporaryRating, setTemporaryRating] = useState<number>(0);
 
   const stars = Array(DEFAULT_COUNT).fill(DEFAULT_ICON);
 
+  const handleMouseEnter = (rating: number) => {
+    if (editable) {
+      setTemporaryRating(rating);
+    }
+  }
+
+  const handleMouseLeave = () => {
+    if (editable) {
+      setTemporaryRating(0);
+    }
+  }
+
   const handleClick = (rating: number) => {
-    setRating(rating);
+    if (editable) {
+      setRating(rating);
+    }
   };
 
   return (
@@ -30,14 +49,14 @@ export default function Ratings() {
 
         return (
           <div
-            className="cursor-pointer text-xl hover:scale-[1.2]"
+            className={`text-xl ${editable && "cursor-pointer hover:scale-[1.2]"}`}
             key={index}
             style={{
               color: elementColor,
               filter: `${active ? "grayscale(0%)" : "grayscale(100%)"}`,
             }}
-            onMouseEnter={() => setTemporaryRating(index + 1)}
-            onMouseLeave={() => setTemporaryRating(0)}
+            onMouseEnter={() => handleMouseEnter(index + 1)}
+            onMouseLeave={handleMouseLeave}
             onClick={() => handleClick(index + 1)}
           >
             {DEFAULT_ICON}
