fc5d34f7a62e22e0c75918073408e53d2fa99972
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-04-16 10:09:44 -0600

    Add a POST handler

--- app/api/restaurants/route.ts
+++ app/api/restaurants/route.ts
@@ -1,8 +1,27 @@
 import clientPromise from "@/utils/connect";
+import { ObjectId } from "mongodb";
 
-export async function GET(request: Request) {
+export interface Restaurant {
+  buyer: string;
+  restaurant: string;
+  time: number;
+}
+
+export interface RestaurantDocument extends Restaurant {
+  _id: ObjectId;
+}
+
+export async function GET() {
   const client = await clientPromise;
-  const findResult = client.db("burrito-db").collection("restaurants").find();
+  const findResult = client.db("next_burrito_club").collection("restaurants").find();
   const restaurants = await findResult.toArray();
   return Response.json(restaurants);
+}
+
+export async function POST(request: Request) {
+  const newRestaurant = await request.json();
+  const client = await clientPromise;
+  const insertResult = await client.db("next_burrito_club").collection("restaurants").insertOne(newRestaurant);
+  const newId = insertResult.insertedId;
+  return Response.json({ success: `Added restaurant with id ${newId}` });
 }
---
+++ components/RestaurantInput.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { useState } from "react";
+import { Restaurant } from "@/app/api/restaurants/route";
+
+function RestaurantInput() {
+  const [restaurant, setRestaurant] = useState<string>("");
+  const [buyer, setBuyer] = useState<string>("");
+
+  const handleSubmit = async () => {
+    if (restaurant !== "" && restaurant.trim().length !== 0 &&
+      buyer !== "" && buyer.trim().length !== 0) {
+      const time: number = Date.now();
+      const newChoice: Restaurant = { buyer, restaurant, time };
+      const response = await fetch("http://localhost:3000/api/restaurants", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newChoice)
+      });
+      const status = await response.json();
+      console.log(status);
+
+      setRestaurant("");
+      setBuyer("");
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      <input
+        autoComplete="off"
+        value={restaurant}
+        placeholder="Restaurant"
+        onChange={(e) => setRestaurant(e.target.value)}
+      />
+      <input
+        autoComplete="off"
+        value={buyer}
+        placeholder="Buyer"
+        onChange={(e) => setBuyer(e.target.value)}
+      />
+      <button
+        onClick={handleSubmit}
+      >
+        Let&apos;s Eat
+      </button>
+    </div>
+  );
+}
+
+export default RestaurantInput;
