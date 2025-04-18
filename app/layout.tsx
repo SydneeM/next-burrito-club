c10f80c48bbe1f23f7fadc65f7d2451cc2942615
Sydnee Mizuno <syd.mizuno@gmail.com>
2025-04-18 15:58:57 -0600

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Noto_Serif } from "next/font/google";
+import { Geist, Geist_Mono, Noto_Serif, Roboto } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 
 const notoSerif = Noto_Serif({
   variable: "--font-noto-serif",
-  subsets: ["latin"],
+  subsets: ["latin"]
+})
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"]
 })
 
 export const metadata: Metadata = {
@@ -31,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${notoSerif.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${notoSerif.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
